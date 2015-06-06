97523523de876e7d050a046d03dd0709c14f90ad
Billy Okal <okal.billy@googlemail.com>
2015-06-07 00:56:12 +0200

    Project re-organization into a full package
    - Re-organized module structure to reflect a more clear package
    - Created necessary setup and package files for development

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+from setuptools.extension import Extension
+import numpy as np
+
+import os
+
+if os.path.exists('MANIFEST'):
+    os.remove('MANIFEST')
+
+setup(name="sirl",
+      version="0.0.1",
+      install_requires=["numpy", "networkx"],
+      packages=['sirl',
+                'sirl.algorithms',
+                'sirl.utils',
+                'sirl.tests',
+                'sirl.algorithms.tests',
+                'sirl.utils.tests'
+                ],
+      include_package_data=True,
+      description="Sample (Graph) Based Inverse Reinforcement Learning",
+      author="Billy Okal",
+      author_email="sudo@makokal.com",
+      url="http://sample-irl.github.io",
+      license="New BSD",
+      use_2to3=True,
+      classifiers=['Intended Audience :: Science/Research',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering',
+                   'Operating System :: Microsoft :: Windows',
+                   'Operating System :: POSIX',
+                   'Operating System :: Unix',
+                   'Operating System :: MacOS',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.3',
+                   ],
+      include_dirs=[np.get_include()]
+      )
