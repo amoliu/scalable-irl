95caabfb758961e6f6d4b457a317528fe2cf7ad0
Billy Okal <okal.billy@googlemail.com>
2015-06-08 00:19:04 +0200

    Making interfaces for models
    - Made interfaces for local controller, reward function and graph mdp

--- sirl/domains/social_navigation.py
+++ sirl/domains/social_navigation.py
@@ -1,33 +1,61 @@
 
 from __future__ import division
 
+import numpy as np
 
-# from ..models import MDPState
 from ..models import LocalController
 from ..models import MDPReward
 from ..models import GraphMDP
 
 
-# Maybe get rid of this because of graph already has the data
-# class SocialNavState(MDPState):
-#     """ Social Navigation MDP state """
-#     def __init__(self, arg):
-#         super(SocialNavState, self).__init__()
-#         self.arg = arg
+########################################################################
 
 
 class SocialNavLocalController(LocalController):
-    """ Social navigation local controller """
-    def __init__(self, arg):
-        super(SocialNavLocalController, self).__init__()
-        self.arg = arg
+    """ Social navigation local controller in 2D space """
+    def __init__(self, kind='linear'):
+        super(SocialNavLocalController, self).__init__(kind)
+
+    def __call__(self, state, action, duration):
+        """ Run a local controller from a state
+
+        Run the local controller at the given ``state`` using the ``action``
+        represented by an angle, :math:` \alpha \in [0, \pi]` for a time limit
+        given by ``duration``
+
+        Parameters
+        -----------
+        state : array like, shape = [1 x 2]
+            2D pose of the start state
+        action : float
+            Angle representing the action taken
+        duration : float
+            Real time interval limit for executing the controller
+
+        Returns
+        --------
+        new_state : 2D array
+            New state reached by the controller
+        """
+        nx = state[0] + np.cos(action) * duration
+        ny = state[1] + np.sin(action) * duration
+        return (nx, ny)
+
+########################################################################
 
 
 class SocialNavReward(MDPReward):
     """ Social Navigation Reward Funtion """
-    def __init__(self, arg):
-        super(SocialNavReward, self).__init__()
-        self.arg = arg
+    def __init__(self, persons, relations, kind='linfa', resolution=0.1):
+        super(SocialNavReward, self).__init__(kind)
+        self._persons = persons
+        self._relations = relations
+        self._resolution = resolution
+
+    def __call__(self, state, action):
+        return 0.0
+
+########################################################################
 
 
 class SocialNavMDP(GraphMDP):
--- sirl/models.py
+++ sirl/models.py
@@ -1,60 +1,61 @@
 
 from __future__ import division
+from abc import abstractmethod, ABCMeta
 
 import numpy as np
 
 from .state_graph import StateGraph
 
 
-# TODO
-# - Move these social nav specific classes to domains module
-# - Create abstract interfaces to define the protocol
-# - Move the GraphMDP to some other place, make an interface for it
-
-# class MDPState(object):
-#     """ GraphMDP State """
-#     def __init__(self, sid, data, cost, priority, Q):
-#         self.sid = sid
-#         self.data = data
-#         self.cost = cost
-#         self.priority = priority
-#         self.Q = Q
-
+########################################################################
 
 class LocalController(object):
-    """ GraphMDP Linear Action (local controller) in 2D """
-    def __init__(self, angle, duration):
-        self.angle = angle
-        self.duration = duration
+    """ GraphMDP local controller """
 
-    def __call__(self, state):
-        """ Run the local controller at the given ``state``"""
-        nx = state.data[0] + np.cos(self.angle) * self.duration
-        ny = state.data[1] + np.sin(self.angle) * self.duration
-        return (nx, ny)
+    __metaclass__ = ABCMeta
 
+    def __init__(self, kind='linear'):
+        self.kind = kind
+
+    @abstractmethod
+    def __call__(self, state, action, duration):
+        raise NotImplementedError('Abstract method')
+
+
+########################################################################
 
 class MDPReward(object):
-    """ Reward represented as linear function approximation"""
-    def __init__(self, weights, persons, relations):
-        self.weights = weights
-        self.persons = persons
-        self.relations = relations
+    """ Reward  function base class """
 
+    __metaclass__ = ABCMeta
+
+    def __init__(self, kind='linfa'):
+        self.kind = kind
+
+    @abstractmethod
     def __call__(self, state, action):
-        pass
+        raise NotImplementedError('Abstract method')
 
+
+########################################################################
 
 class GraphMDP(object):
-    """ Adaptive State-Graph MDP representation """
-    def __init__(self, discount, reward, transition):
+    """ Adaptive State-Graph MDP """
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, discount, reward, controller):
         self.discount = discount
         self.reward = reward
-        self.transition = transition
+        self.controller = controller
 
-    # make abstract to depend on domain
+        # setup the graph structure
+        self._g = StateGraph()
+
+    @abstractmethod
     def initialize_state_graph(self, init_samples):
-        pass
+        """ Initialize graph using set of initial samples """
+        raise NotImplementedError('Abstract method')
 
     def run(self, max_states):
         pass
