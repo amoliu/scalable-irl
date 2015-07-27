ed21d07511fe949e7f4f7d95126eea10abf37714
Billy Okal <okal.billy@googlemail.com>
2015-07-27 21:52:29 +0200

    refactor posq controller code for PEPness

--- sirl/domains/navigation/local_controllers.py
+++ sirl/domains/navigation/local_controllers.py
@@ -114,8 +114,6 @@ class POSQLocalController(LocalController):
                 self._wconfig.y < ny < self._wconfig.h:
             start = np.array([state[0], state[1], 0])
             target = np.array([nx, ny, np.pi/2])
-
-            print('Running posq with: {}, {}'.format(start, target))
             traj = self.trajectory(start, target, max_speed)
             return target, traj
 
@@ -128,50 +126,48 @@ class POSQLocalController(LocalController):
 
         traj, speedvec, vel, inct = self._posq_integrate(
             start, target, direction, self._resolution,
-            self._base, initT, nS=0)
+            self._base, initT, max_speed, nS=0)
 
         return traj
 
-    def _posq_integrate(self, xstart, xend, direction, deltaT, b, initT, nS=0):
+    def _posq_integrate(self, xstart, xend, direction, deltaT,
+                        b, initT, vmax, nS=0):
         """ POSQ Integration procedure to general full trajectory """
         assert xstart.shape == xend.shape, 'Expect similar vector sizes'
         assert xstart.size == xend.size == 3, 'Expect 1D array (x, y, theta)'
 
-        # if size(xstart) == [1, 3], xstart = xstart.T
-        # if size(xend)   == [1, 3], xend   = xend.T
         vel = np.zeros(shape=(1, 2))
-        # Initialize variables
         sl, sr = 0, 0
-        oldSl, oldSr = 0, 0
+        old_sl, old_sr = 0, 0
         xvec = np.zeros(shape=(1, 3))  # pose vectors for trajectory
         speedvec = np.zeros(shape=(1, 2))  # velocities during trajectory
         encoders = [0, 0]
         t = initT  # initialize global timer
         ti = 0  # initialize local timer
         eot = 0  # initialize end-of-trajectory flag
-        xcurrent = [xstart[0], xstart[1], xstart[2]]
-        oldBeta = 0
+        xnow = [xstart[0], xstart[1], xstart[2]]
+        old_beta = 0
 
         while not eot:
             # Calculate distances for both wheels
-            dSl = sl - oldSl
-            dSr = sr - oldSr
+            dSl = sl - old_sl
+            dSr = sr - old_sr
             dSm = (dSl + dSr) / 2
-            dSd = (dSr - dSl) / b
+            dSd = (dSr - dSl) / self._base
 
             # Integrate robot position
-            xcurrent[0] = xcurrent[0] + dSm * np.cos(xcurrent[2] + dSd / 2.0)
-            xcurrent[1] = xcurrent[1] + dSm * np.sin(xcurrent[2] + dSd / 2.0)
-            xcurrent[2] = normangle(xcurrent[2] + dSd, -np.pi)
+            xnow[0] = xnow[0] + dSm * np.cos(xnow[2] + dSd / 2.0)
+            xnow[1] = xnow[1] + dSm * np.sin(xnow[2] + dSd / 2.0)
+            xnow[2] = normangle(xnow[2] + dSd, -np.pi)
 
             # implementation of the controller
-            vl, vr, eot, vm, vd, oldBeta = self._posq_step(ti, xcurrent, xend,
-                                                           direction, b,
-                                                           oldBeta)
+            vl, vr, eot, vm, vd, old_beta = self._posq_step(ti, xnow, xend,
+                                                            direction,
+                                                            old_beta, vmax)
             vel = np.row_stack((vel, [vm, vd]))
             speeds = np.array([vl, vr])
             speedvec = np.row_stack((speedvec, speeds))
-            xvec = np.row_stack((xvec, xcurrent))
+            xvec = np.row_stack((xvec, xnow))
 
             # Increase timers
             ti = ti + deltaT
@@ -184,11 +180,9 @@ class POSQLocalController(LocalController):
             encoders[0] += delta_dist1
             encoders[1] += delta_dist2
 
-            # print(encoders)
-
             # Keep track of previous wheel positions
-            oldSl = sl
-            oldSr = sr
+            old_sl = sl
+            old_sr = sr
 
             # noise on the encoders
             sl = encoders[0] + nS * np.random.uniform(0, 1)
@@ -196,77 +190,70 @@ class POSQLocalController(LocalController):
 
         inct = t  # at the end of the trajectory the time elapsed is added
 
-        return [xvec, speedvec, vel, inct]
+        return xvec, speedvec, vel, inct
 
-    def _posq_step(self, t, xcurrent, xend, direction, b, oldBeta):
-        Kv = 1
-        Krho = 3    # Condition: Kalpha + 5/3*Kbeta - 2/pi*Krho > 0 !
-        Kalpha = 1
-        Kbeta = -1
-        Vmax = Krho                # [m/s]
-        RhoEndCondition = 0.0510      # [m]
+    def _posq_step(self, t, xnow, xend, direction, old_beta, vmax):
+        k_v = 5.9
+        k_rho = 0.2    # Condition: k_alpha + 5/3*k_beta - 2/pi*k_rho > 0 !
+        k_alpha = 0.91
+        k_beta = -1
+        rho_end = 0.0510      # [m]
 
         if t == 0:
-            oldBeta = 0
+            old_beta = 0
 
         # extract coordinates
-        xc, yc, tc = xcurrent[0], xcurrent[1], xcurrent[2]
+        xc, yc, tc = xnow[0], xnow[1], xnow[2]
         xe, ye, te = xend[0], xend[1], xend[2]
 
         # rho
         dx = xe - xc
         dy = ye - yc
         rho = np.sqrt(dx**2 + dy**2)
-        fRho = rho
-        if fRho > (Vmax / Krho):
-            fRho = Vmax / Krho
+        f_rho = rho
+        if f_rho > (vmax / k_rho):
+            f_rho = vmax / k_rho
 
         # alpha
-        alpha = np.arctan2(dy, dx) - tc
-        alpha = normangle(alpha, -np.pi)
+        alpha = normangle(np.arctan2(dy, dx) - tc, -np.pi)
 
         # direction (forward or backward)
         if direction == 0:
             if alpha > np.pi / 2:
-                fRho = -fRho                   # backwards
+                f_rho = -f_rho                   # backwards
                 alpha = alpha - np.pi
             elif alpha <= -np.pi / 2:
-                fRho = -fRho                   # backwards
+                f_rho = -f_rho                   # backwards
                 alpha = alpha + np.pi
         elif direction == -1:                  # arrive backwards
-            fRho = -fRho
+            f_rho = -f_rho
             alpha = alpha + np.pi
             if alpha > np.pi:
                 alpha = alpha - 2 * np.pi
 
-        # phi
+        # phi, beta
         phi = te - tc
         phi = normangle(phi, -np.pi)
-
         beta = normangle(phi - alpha, -np.pi)
-        if abs(oldBeta - beta) > np.pi:           # avoid instability
-            beta = oldBeta
-        oldBeta = beta
+        if abs(old_beta - beta) > np.pi:           # avoid instability
+            beta = old_beta
+        old_beta = beta
 
-        # New version
-        vm = Krho * np.tanh(fRho * Kv)
-        vd = (Kalpha * alpha + Kbeta * beta)
-        eot = (rho < RhoEndCondition)
+        vm = k_rho * np.tanh(f_rho * k_v)
+        vd = (k_alpha * alpha + k_beta * beta)
+        eot = (rho < rho_end)
 
         if eot:
             print('t:{}  x:{}  y:{}  theta:{}'
                   .format(t, xc, yc, tc * 180 / np.pi))
 
-        # print('t:{}  x:{}  y:{}  theta:{}, oldBeta:{}'
-        #       .format(t, xc, yc, tc * 180 / np.pi, oldBeta))
-
         # Convert speed to wheel speeds
-        vl = vm - vd * b / 2
-        if abs(vl) > Vmax:
-            vl = Vmax * np.sign(vl)
+        vl = vm - vd * self._base / 2
+        if abs(vl) > vmax:
+            vl = vmax * np.sign(vl)
 
-        vr = vm + vd * b / 2
-        if abs(vr) > Vmax:
-            vr = Vmax * np.sign(vr)
+        vr = vm + vd * self._base / 2
+        if abs(vr) > vmax:
+            vr = vmax * np.sign(vr)
 
-        return vl, vr, eot, vm, vd, oldBeta
+        return vl, vr, eot, vm, vd, old_beta
